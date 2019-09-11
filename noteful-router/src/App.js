9892dcf4fe039fc890c3fba74c014745bd21681a
James Lee <jameswlee109@gmail.com>
2019-09-11 10:41:33 -0500

    component files created

--- noteful-router/src/App.js
+++ noteful-router/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header/Header'
+import SideFolders from './SideFolders/SideFolders'
+import NoteList from './NoteList/NoteList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
+        <SideFolders />
+        <NoteList />
       </div>
     );
   }
